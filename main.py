7995cb0965f70e82fac180ca1fa2cb0e22f2c30b
Brandon Saraiva <brandon.1610@hotmail.com>
2023-10-27 05:34:37 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,29 +2,27 @@ from funcoes.puttingDocsIntoElastic import indexar_documentos_no_elasticsearch
 
 from funcoes.index_name import formatar_nome_indice_a_partir_url
 
+from funcoes.modal import modal_input_infos
+
 def main():
-    # Coloque aqui o URL do arquivo JSON
-    json_url = "https://sistemas.anac.gov.br/dadosabertos/regulamentacao/Normas%20Publicadas/resolucoes/2022.json"
+    info = {
+        "UrlJson": "https://sistemas.anac.gov.br/dadosabertos/fiscalizacao/decisoes-monocraticas-de-processos-em-segunda-instancia/2022/2022.json",
 
-# apartir depois de qual ponto do url que voce quer pegar o nome do index
-    split_point = "dadosabertos/"
+        "IndexName": "nome do index aqui",
+        
+        "ColumnName": "nome da coluna aqui"
+    }
 
+    infos_atts = modal_input_infos(info)
+    
 # FIltrando o nome da url
-    index_name = formatar_nome_indice_a_partir_url(json_url, split_point)
-
-    # putting the index_name to lower case
-    index_name = index_name.lower()
-
-    print("\nNome do index: ", index_name);
-
-    # Coluna que contem os links para os documentos
-    column_name = "outros"
 
     # http do seu servid Elasticsearch
-    elastic_host = "http://localhost:9200"
+    elastic_host = "http do seu elastic aqui"
+
 
     # Indexando os documentos no Elasticsearch
-    indexar_documentos_no_elasticsearch(json_url, index_name, column_name, elastic_host)
+    indexar_documentos_no_elasticsearch(infos_atts["UrlJson"], infos_atts["IndexName"], infos_atts["ColumnName"], elastic_host)
 
 if __name__ == "__main__":
     main()
